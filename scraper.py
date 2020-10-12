3e4836d55a80bf5a0578c997f576ef93843fa0b8
Akshaj Dev <62797898+akshajdevkv@users.noreply.github.com>
2020-10-12 15:03:56 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,7 +5,7 @@ import webbrowser
 
 headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0'}
 
-url = 'http://opensource.guide/starting-a-project/'
+url = 'URL'
 r = requests.get(url,headers = headers)
 soup = BeautifulSoup(r.text,'html.parser')
 heading = soup.find('h1')
@@ -21,10 +21,10 @@ for i in raw_contents:
     else:
         contents += str(i)
  
-with open('/home/akshajdev/Desktop/Python Projects/Article Simplifier/template.html','r') as f:
+with open('path of template.html','r') as f:
     html_content = f.read() 
     t = Template(html_content)
 
-with open('/home/akshajdev/Desktop/Python Projects/Article Simplifier/original.html','w') as f:
+with open('path of output.html','w') as f:
     f.write(t.safe_substitute({'content': contents,'title':heading.text,'heading':heading.text,'sub_heading':sub_heading.text}))
 
