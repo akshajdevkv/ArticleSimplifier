0dba8e585a6d912e2b1a7ceb835c395e04fa55c6
Akshaj Dev <akshajtechtips@gmail.com>
2020-10-13 12:54:05 +0530

    Update

--- Websites/mediumscraper.py
+++ Websites/mediumscraper.py
@@ -5,7 +5,8 @@ import os
 
 output_path = os.environ.get('output_path')
 template_path = os.environ.get('template_path')
-url = 'https://medium.com/@ztrana/the-expert-generalist-why-the-future-belongs-to-polymaths-46b0e9edc7bc'
+
+url = 'https://medium.com/illumination-curated/elon-musks-one-rule-for-learning-smarter-faster-and-better-b08bb30499'
 r = requests.get(url)
 soup = BeautifulSoup(r.text,'html.parser')
 heading = soup.find('h1')
@@ -16,7 +17,6 @@ for i in soup.find_all(['p','ul','ol','h1','h2','blockquote','figure'])[3:-20]:
     if '<figure' in str(i):
         image  = i.find('img') 
         content += str(image).replace('/60/','/1000/').replace('width','id').replace('height','class')
-        
     else:
         content += str(i)
  
